3b954d49e91b76baf795f0528c683cb35690b6d2
Danilo Tosic <54001767+ToskePG@users.noreply.github.com>
2025-01-05 20:39:26 +0100

    Kreira Listu. Dodaje funkcije isEmpty i dodavanje na pocetak i kraj liste.

--- src/mainPackage/CarStore.java
+++ src/mainPackage/CarStore.java
@@ -2,4 +2,44 @@ package mainPackage;
 
 public class CarStore {
 
+	private String storeName;
+	private Node head;
+	
+	CarStore(String storeName){
+		this.storeName = storeName;
+		this.head = null;
+	}
+	
+	// Provjerava da li je lista prazna. Lista je prazna ako je head = null. Nema provjere da li je lista puna, jer je lista dinamicka struktura koja nikad nije puna.
+	public boolean isEmpty() {
+		return this.head == null;
+	}
+	
+	// Dodavanje na pocetak.
+	public boolean addAtBegging(Car c1) {
+		Node newNode = new Node(c1);
+		if(isEmpty()) {
+			head = newNode;
+			return true;
+		}
+		Node temp = head;
+		head = newNode;
+		head.setNext(temp);
+		return true;
+	}
+	
+	// Dodavanje na kraj (Ukoliko u testu nije napomenuto nista dodatno, dodavanje se podrazumijeva da bude na kraj liste!)
+	public boolean addAtEnd(Car c1) {
+		Node newNode = new Node(c1);
+		if(isEmpty()) {
+			head = newNode;
+			return true;
+		}
+		Node temp = head;
+		while(temp != null) {
+			temp = temp.getNext();
+		}
+		temp = newNode;
+		return true;
+	}
 }
--- src/mainPackage/Node.java
+++ src/mainPackage/Node.java
@@ -1,5 +1,34 @@
 package mainPackage;
 
 public class Node {
+	
+	private Car data;
+	private Node next;
+	
+	Node(Car car){
+		this.data = car;
+		this.next = null;
+	}
 
+	public Car getData() {
+		return data;
+	}
+
+	public void setData(Car data) {
+		this.data = data;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [data=" + data + ", next=" + next + "]";
+	}
+	
 }
